005b803a576107c529a45e857611d08cecb4e5d8
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-09-06 07:07:17 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,30 @@
+/*
+скрипт, який для кожного елемента масиву ingredients:
+
+Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+Додасть назву інгредієнта як його текстовий вміст.
+Додасть елементу клас item.
+Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+*/
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListRef = document.querySelector("#ingredients");
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+
+  item.classList.add("item");
+  item.textContent = ingredient;
+
+  return item;
+});
+
+ingredientsListRef.append(...ingredientsItems);
